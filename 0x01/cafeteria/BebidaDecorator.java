8b07abcd52eb7e8099f600f3c98f3db60e05d19b
Jenifer <jnakabar@ciandt.com>
2022-08-18 21:24:50 -0300

    adicionando task 1

---
+++ 0x01/cafeteria/BebidaDecorator.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BebidaDecorator extends Bebida{
+    private final Bebida bebidaDecorada;
+
+    public BebidaDecorator(Bebida bebidaDecorada) {
+        this.bebidaDecorada = bebidaDecorada;
+    }
+
+    @Override
+    public List<String> obterIngredientes() {
+         return this.bebidaDecorada.obterIngredientes();
+    }
+
+    @Override
+    public double obterPreco() {
+        return this.bebidaDecorada.obterPreco();
+    }
+}
