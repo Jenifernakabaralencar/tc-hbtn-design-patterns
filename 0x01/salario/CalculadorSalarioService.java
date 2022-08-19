7eea1ed7087491dcd2eab7d9b89eb61b83b26a08
Jenifer <jnakabar@ciandt.com>
2022-08-18 21:05:05 -0300

    Adicionando task 0

--- 0x01/salario/CalculadorSalarioService.java
+++ 0x01/salario/CalculadorSalarioService.java
@@ -17,4 +17,12 @@ public class CalculadorSalarioService {
     public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
         return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
     }
+
+    public static CalculadorSalarioService getInstancia() {
+        return instancia;
+    }
+
+    public static void setInstancia(CalculadorSalarioService instancia) {
+        CalculadorSalarioService.instancia = instancia;
+    }
 }
